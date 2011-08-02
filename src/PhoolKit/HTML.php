4bc43a049dcc3f78d38071ce223419eb958f253b
Klaus Reimer <k@ailis.de>
2011-08-02 21:48:53 +0200

    Added support for checkboxes, radio buttons and select options.

--- src/PhoolKit/HTML.php
+++ src/PhoolKit/HTML.php
@@ -22,18 +22,18 @@ namespace PhoolKit;
 /**
  * Helper methods for printing certain HTML snippets. It is recommended to
  * inlude this file in all pages and create an alias for it:
- * 
+ *
  * <pre>
  * use PhoolKit/HTML as h;
  * </pre>
- * 
+ *
  * Then the methods of this class can be accessed in a very short way:
- * 
+ *
  * <pre>
  * <?h::text("Some text to escape")?>
  * <?h::url("login")?>
  * </pre>
- * 
+ *
  * This class is not final so you can use it as a base class for your own
  * HTML helper classes.
  *
@@ -43,7 +43,10 @@ class HTML
 {
     /** The currently bound form. */
     private static $form = NULL;
-    
+
+    /** If auto focus was already set in a form. */
+    private static $alreadySetAutoFocus;
+
     /**
      * Private constructor to prevent instantiation.
      */
@@ -63,7 +66,7 @@ class HTML
     {
         echo htmlspecialchars($text);
     }
-    
+
     /**
      * Prints the specified text into the JavaScript output. The text is
      * property escaped.
@@ -75,7 +78,7 @@ class HTML
     {
         echo StringUtils::escapeJS($text);
     }
-    
+
     /**
      * Prints the specified URL to the HTML output in a resolved form. The
      * URL must be relative to the web root and this method takes care of
@@ -102,7 +105,7 @@ class HTML
     /**
      * Same as the url() method but intended to be used inside of javascript
      * blocks. It is properly escaped.
-     * 
+     *
      * @param string $url
      *            The URL to print. If not specified then only the base
      *            directory is printed (Useful for linking to the root index
@@ -117,33 +120,37 @@ class HTML
             echo "/";
         }
         echo StringUtils::escapeJS($url);
-    }    
-    
+    }
+
     /**
      * Prints the attributes to be put into the form tag for the currently
-     * bound form. 
+     * bound form.
      */
     public final static function form()
     {
-        echo "novalidate onsubmit=\"return new phoolkit.Form(this).submit(function(){\n";
+        echo "onsubmit=\"return new phoolkit.Form(this).submit(function(){\n";
         foreach (self::getForm()->getValidators() as $validator)
         {
             echo htmlspecialchars($validator->getScript());
         }
         echo "})\"";
+
+        // Reset form specific flags
+        self::$alreadySetAutoFocus = false;
     }
-    
+
     /**
-     * Prints the attributes for initially autofocusing a form field. 
+     * Prints the attributes for initially autofocusing a form field.
      */
     public final static function autoFocus()
     {
         if (!self::getForm()->hasErrors()) echo " autofocus";
     }
-    
+
     /**
-     * Prints the attributes for a form input field.
-     * 
+     * Prints the attributes for a normal form input field (text, password and
+     * hidden).
+     *
      * @param string $name
      *            The form field name.
      * @param string $id
@@ -151,13 +158,11 @@ class HTML
      */
     public final static function input($name, $id = NULL)
     {
-        static $alreadySetAutoFocus;
-        
         $form = self::getForm();
         $value = $form->readProperty($name);
         if (!$id) $id = $name;
-        $setAutoFocus = !$alreadySetAutoFocus && $form->hasErrors($name);
-        if ($setAutoFocus) $alreadySetAutoFocus = $setAutoFocus;
+        $setAutoFocus = !self::$alreadySetAutoFocus && $form->hasErrors($name);
+        if ($setAutoFocus) self::$alreadySetAutoFocus = $setAutoFocus;
         printf("id=\"%s\" name=\"%s\" value=\"%s\"%s",
             htmlspecialchars($id),
             htmlspecialchars($name),
@@ -165,17 +170,110 @@ class HTML
             $setAutoFocus ? " autofocus" : ""
         );
     }
-    
+
     /**
-     * Prints the HTML code for the error list of a form field.
-     * 
+     * Prints the attributes for a form checkbox input field.
+     *
      * @param string $name
      *            The form field name.
-     */  
+     * @param string $id
+     *            Optional ID. Defaults to field name.
+     */
+    public final static function checkbox($name, $id = NULL)
+    {
+        $form = self::getForm();
+        $value = $form->readProperty($name);
+        if (!$id) $id = $name;
+        $setAutoFocus = !self::$alreadySetAutoFocus && $form->hasErrors($name);
+        if ($setAutoFocus) self::$alreadySetAutoFocus = $setAutoFocus;
+        printf("type=\"checkbox\" id=\"%s\" name=\"%s\" value=\"1\"%s%s",
+            htmlspecialchars($id),
+            htmlspecialchars($name),
+            $value ? " checked" : "",
+            $setAutoFocus ? " autofocus" : ""
+        );
+    }
+
+    /**
+     * Prints the attributes for a form radio input field.
+     *
+     * @param string $name
+     *            The form field name.
+     * @param string $value
+     *            The value of the radio button.
+     * @param string $id
+     *            Optional ID. Defaults to field name.
+     */
+    public final static function radio($name, $value, $id = NULL)
+    {
+        $form = self::getForm();
+        $realValue = $form->readProperty($name);
+        if (!$id) $id = $name . "-" . $value;
+        $setAutoFocus = !self::$alreadySetAutoFocus && $form->hasErrors($name);
+        if ($setAutoFocus) self::$alreadySetAutoFocus = $setAutoFocus;
+        printf("type=\"radio\" id=\"%s\" name=\"%s\" value=\"%s\"%s%s",
+            htmlspecialchars($id),
+            htmlspecialchars($name),
+            htmlspecialchars($value),
+            $value == $realValue ? " checked" : "",
+            $setAutoFocus ? " autofocus" : ""
+        );
+    }
+
+    /**
+     * Prints the attributes for a form select field.
+     *
+     * @param string $name
+     *            The form field name.
+     * @param string $id
+     *            Optional ID. Defaults to field name.
+     */
+    public final static function select($name, $id = NULL)
+    {
+        $form = self::getForm();
+        if (!$id) $id = $name;
+        $setAutoFocus = !self::$alreadySetAutoFocus && $form->hasErrors($name);
+        if ($setAutoFocus) self::$alreadySetAutoFocus = $setAutoFocus;
+        printf(" id=\"%s\" name=\"%s\"%s",
+            htmlspecialchars($id),
+            htmlspecialchars($name),
+            $setAutoFocus ? " autofocus" : ""
+        );
+    }
+
+    /**
+     * Prints the options for a form select field.
+     *
+     * @param $name
+     *            The field name.
+     * @param $options
+     *            A map with the options. Map key is the value used in the
+     *            form. Map value is the displayed text.
+     */
+    public final static function options($name, $options)
+    {
+        $form = self::getForm();
+        $realValue = $form->readProperty($name);
+        foreach ($options as $value => $caption)
+        {
+            printf("<option value=\"%s\"%s>%s</option>\n",
+                htmlspecialchars($value),
+                $value == $realValue ? " selected" : "",
+                htmlspecialchars($caption)
+            );
+        }
+    }
+
+    /**
+     * Prints the HTML code for the error list of a form field.
+     *
+     * @param string $name
+     *            The form field name.
+     */
     public final static function errors($name = NULL)
     {
         $errors = $name ? self::getForm()->getErrors($name) : array();
-        printf("<ul class=\"errors%s\">", 
+        printf("<ul class=\"errors%s\">",
             $name ? " " . htmlspecialchars($name) : "");
         foreach ($errors as $error)
         {
@@ -184,11 +282,11 @@ class HTML
             echo "</li>";
         }
         echo "</ul>";
-    }    
-    
+    }
+
     /**
      * Binds the specified form.
-     * 
+     *
      * @param Form $form
      *           The form to bind.
      */
@@ -199,7 +297,7 @@ class HTML
 
     /**
      * Returns the bound form. If no form is bound then an exception is thrown.
-     * 
+     *
      * @return Form
      *            The bound form. Never null.
      * @throws LogicException
@@ -210,5 +308,69 @@ class HTML
         if (!self::$form)
             throw new LogicException("No form bound to HTML");
         return self::$form;
+    }
+
+    /**
+     * Resolves a message key into a message and prints it. The output is
+     * HTML escaped.
+     *
+     * @param string $key
+     *            The message key.
+     * @param mixed $args___
+     *            Variable number of optional arguments used by the message.
+     * @return The resolved message.
+     */
+    public final static function msg($key, $args___ = NULL)
+    {
+        echo htmlspecialchars(call_user_func_array(
+            array("\PhoolKit\I18N", "getMessage"), func_get_args()));
+    }
+
+    /**
+     * Resolves a message key into a message and prints it. The output is
+     * JavaScript escaped.
+     *
+     * @param string $key
+     *            The message key.
+     * @param mixed $args___
+     *            Variable number of optional arguments used by the message.
+     * @return The resolved message.
+     */
+    public final static function jsMsg($key, $args___ = NULL)
+    {
+        echo StringUtils::escapeJS(call_user_func_array(
+            array("\PhoolKit\I18N", "getMessage"), func_get_args()));
+    }
+
+    /**
+     * Resolves a message key into a message and prints it. The output is
+     * not escaped in any way. Use this for messages which might contain
+     * HTML tags and you are sure it doesn't harm your website.
+     *
+     * @param string $key
+     *            The message key.
+     * @param mixed $args___
+     *            Variable number of optional arguments used by the message.
+     * @return The resolved message.
+     */
+    public final static function rawMsg($key, $args___ = NULL)
+    {
+        echo StringUtils::escapeJS(call_user_func_array(
+            array("\PhoolKit\I18N", "getMessage"), func_get_args()));
+    }
+
+    /**
+     * Shortcut for Request::getParam()
+     *
+     * @param string $name
+     *            The parameter name.
+     * @param string $default
+     *            Optional default value.
+     * @return string
+     *            The parameter value or the default value.
+     */
+    public final static function param($name, $default = NULL)
+    {
+        return Request::getParam($name, $default);
     }
 }
